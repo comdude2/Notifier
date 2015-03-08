1d8b71ada73c9b0828a7e8b2bbb9add01fd3c96a
Matt <Matt@Matt-Desktop-PC>
2015-03-08 09:45:09 +0000

    Added class to make the inventory GUI

---
+++ src/net/mcviral/dev/plugins/notifier/main/Notification.java
@@ -0,0 +1,15 @@
+package net.mcviral.dev.plugins.notifier.main;
+
+import java.util.UUID;
+
+public class Notification {
+	
+	private UUID uuid = null;
+	private String name = null; //the name of the notification
+	private String[] description = null; //the description (lore)
+	
+	public Notification(){
+		
+	}
+	
+}
--- src/net/mcviral/dev/plugins/notifier/main/Notifier.java
+++ src/net/mcviral/dev/plugins/notifier/main/Notifier.java
@@ -1,7 +1,21 @@
 package net.mcviral.dev.plugins.notifier.main;
 
+import java.util.UUID;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Notifier extends JavaPlugin{
-
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	public boolean newNotification(UUID uuid){
+		return false;
+	}
+	
 }
